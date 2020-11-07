6f7364d6aa06cf6b8425218f97084b5ef325f653
TITanZK <503838819@qq.com>
2020-11-07 15:56:03 +0800

    html plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: './src/index.js',
     output: {
         filename: '[name].[contenthash].js',
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'FH - 芳华',
+        template: "src/assets/index.html"
+    })]
 }
+
