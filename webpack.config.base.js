f5b31631755c223d527f5b6e3328c8db9ddef4c2
TITanZK <503838819@qq.com>
2020-11-08 21:06:33 +0800

    webpack dev prod

---
+++ webpack.config.base.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'FH - 芳华',
+            template: "src/assets/index.html"
+        })
+    ]
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,19 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const base  = require('./webpack.config.base')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: '[name].[contenthash].js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'FH - 芳华',
-            template: "src/assets/index.html"
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].[contenthash].css',
-            chunkFilename: '[id].[contenthash].css',
-            ignoreOrder: false,
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                // use: ['style-loader', 'css-loader'],
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {
-                            publicPath: '../',
-                        },
-                    },
-                    'css-loader',
-                ],
-            },
-        ],
-    },
+    ...base,
     devtool: 'inline-source-map',
     devServer: {
         contentBase: './dist',
     },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
+        ],
+    }
 }
 
